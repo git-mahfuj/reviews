ba8fda4e7feb6c6cea9187d52b280f76e226f259
git-mahfuj <mahfujurrheeck@gmail.com>
2025-05-10 13:43:36 +0600

    changed-1

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-document.addEventListener("DOMContentLoaded" , () => {
+
     const items =[
         {
            id: 0,
            name: "The Autodesk 2025 State of Design & Make Report is here",
            dsc:"Discover the latest insights from industry leaders leveraging emerging technologies to help build a more resilient world made for all.",
-           img:"https://damassets.autodesk.net/content/dam/autodesk/www/images/company/sdm-report-2025.jpg"
+           img:"/sdm-report-2025.avif"
         },
 
          {
-           id: 0,
-           name: "The Autodesk 2025 State of Design & Make Report is here",
-           dsc:"Discover the latest insights from industry leaders leveraging emerging technologies to help build a more resilient world made for all.",
-           img:"https://damassets.autodesk.net/content/dam/autodesk/www/images/company/ramboll-bim-gis.jpg"
+           id: 1,
+           name: "Bringing technology and new workflows together to score goals",
+           dsc:"Ramboll leverages BIM and GIS to increase better collaboration between design and environmental teams for the Wrexham A.F.C. Stadium project.",
+           img:"/ramboll-bim-gis.avif"
         },
 
            {
-           id: 0,
-           name: "The Autodesk 2025 State of Design & Make Report is here",
-           dsc:"Discover the latest insights from industry leaders leveraging emerging technologies to help build a more resilient world made for all.",
-           img:"https://damassets.autodesk.net/content/dam/autodesk/www/images/company/india-apprenticeship.jpg"
+           id: 2,
+           name: "How Autodesk’s apprenticeship program shapes future careers",
+           dsc:"Launched in Pakistan, Autodesk’s apprenticeship program bridges the gap between education and the workplace, providing a comprehensive learning experience.",
+           img:"/india-apprenticeship.avif"
         },
 
           {
-           id: 0,
-           name: "The Autodesk 2025 State of Design & Make Report is here",
-           dsc:"Discover the latest insights from industry leaders leveraging emerging technologies to help build a more resilient world made for all.",
-           img:"https://damassets.autodesk.net/content/dam/autodesk/www/Company/overview/autodesks-mission-is-to-empower-innovators-thumb-1172x660.jpg"
+           id: 3,
+           name: "Autodesk's mission is to empower everyone, everywhere to design and make anything",
+           dsc:"Our technology spans architecture, engineering, and construction; product design and manufacturing; and media and entertainment, empowering Design.",
+           img:"/autodesks-mission-is-to-empower-innovators-thumb-1172x660.avif"
         },
 
         {
-           id: 0,
-           name: "The Autodesk 2025 State of Design & Make Report is here",
-           dsc:"Discover the latest insights from industry leaders leveraging emerging technologies to help build a more resilient world made for all.",
-           img:"https://damassets.autodesk.net/content/dam/autodesk/www/Company/overview/work-fluidly-across-boundaries-thumb-1172x660.jpg"
+           id: 4,
+           name: "We equip our customers to work fluidly across boundaries of project, discipline, and industry",
+           dsc:"Over the past four decades, millions of people have trusted our Design and Make technology to transform how their products are made. In the process, we’ve transformed what can be made.",
+           img:"/work-fluidly-across-boundaries-thumb-1172x660.avif"
         },
     ];
 
@@ -43,19 +43,42 @@ document.addEventListener("DOMContentLoaded" , () => {
     const nextBtn = document.getElementById("next-Btn");
     const prevBtn = document.getElementById("prev-Btn");
 
+    let currentItem =0;
+   
+   
+    window.addEventListener("click" , ()=> {
+      showContent(currentItem);
+    })
+
+
     function showContent(person) {
-       const reviews = items[person];
-       contentImg.src = reviews.src;
+      const reviews = items[person];
+       contentImg.src = reviews.img;
        contentName.textContent = reviews.name;
        contentDsc.textContent = reviews.dsc;
         
     }
 
+
     nextBtn.addEventListener("click" , () => {
-      const hek = showContent()
-      console.log(hek)
+      currentItem ++ ;
+      if(currentItem > items.length -1) {
+         currentItem = 0;
+         showContent(currentItem);
+      }
+
+    })
+
+     prevBtn.addEventListener("click" , () => {
+      currentItem -- ;
+      if(currentItem < items.length -4) {
+         currentItem = 4;
+         showContent(currentItem);
+      }
+
     })
 
 
 
-});
+
+
